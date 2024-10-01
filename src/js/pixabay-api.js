37246df883736bd5bac00c02814e715d4e53724a
Kudrytska <tdolinkina@gmail.com>
2024-10-01 14:30:30 +0200

    fix api one

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,17 +2,17 @@ import axios from "axios";
 
 export async function fetchImages(inputValue, page, limit) {
     try {
-        const response = await axios.get(`https://pixabay.com/api/?`, {
-            params: {
-                key: '46126545-8899f9a6fbc888edd135bf332',
-                q: inputValue,
-                image_type: 'photo',
-                orientation: 'horizontal',
-                safesearch: 'true',
-                per_page: limit,
-                page: page,
-            }
+        const searchParams = new URLSearchParams({
+            key: '46126545-8899f9a6fbc888edd135bf332',
+            q: inputValue,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: 'true',
+            per_page: limit,
+            page: page,
         });
+
+        const response = await axios.get(`https://pixabay.com/api/?${searchParams}`);
 
         return response.data.hits;
     } catch (error) {
